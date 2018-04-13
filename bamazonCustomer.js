4ea357ac1ce1d582b57942c1e7509185f7a5072f
Roger Camero <rogercamero@icloud.com>
2018-04-12 18:22:01 -0700

    homework turn in push. working bamazonCustomer.js file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,94 +8,73 @@ var config = mysql.createConnection({
   socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
 })
 
-// config.connect(function (e) {
-//   if (e) throw e
-// })
+function validateInput(value) {
+  var integer = Number.isInteger(parseFloat(value))
+  var sign = Math.sign(value)
+
+  if (integer && (sign === 1)) {
+    return true
+  } else {
+    return 'ERROR: Please enter a correct numeric value.'
+  }
+}
+
+function selectInventory() {
+  config.query('SELECT * FROM products', function (e, res) {
+    if (e) throw e
+    console.log('Welcome to Bamazon! Here is what we have in stock:')
+    console.log("-------------------------------------------------------\n")
+    for (var i = 0; i < res.length; i++) {
+      console.log('ID: ' + res[i].item_id + ' | ' + 'PRODUCT: ' + res[i].product_name + ' | ' + 'DEPARTMENT: ' + res[i].department_name +
+        ' | ' + 'PRICE: ' + res[i].price + ' | ' + 'QTY: ' + res[i].stock_quantity + '\n')
+    }
+
+    console.log("-------------------------------------------------------\n")
+    inquirePurchase()
+  })
+}
+
+function inquirePurchase() {
+  inquire.prompt([{
+      type: 'input',
+      name: 'id',
+      message: 'What is the ID of the product you would like to buy?',
+      validate: validateInput,
+      filter: Number
+    },
+    {
+      type: 'input',
+      name: 'quantity',
+      message: 'How many would you like to purchase?',
+      validate: validateInput,
+      filter: Number
+    }
+  ]).then(function (input) {
+    var quantityDesired = input.quantity
+    var item = input.id
+    makePurchase(item, quantityDesired)
+  })
+
+  function makePurchase(id, quantityNeeded) {
+    config.query('SELECT * FROM products WHERE item_id = ' + id, function (e, res) {
+      if (e) throw e
+
+      if (quantityNeeded <= res[0].stock_quantity) {
+        var total = res[0].price * quantityNeeded
+        console.log('Thank you for your order')
+        console.log("Your total cost for " + quantityNeeded + " " + res[0].product_name + " is " + total + ".")
+        config.query("UPDATE products SET stock_quantity = stock_quantity - " + quantityNeeded + " WHERE item_id = " + id)
+        // config.end()
+      } else {
+        console.log('Insufficient quantity! Please change your order.')
+        selectInventory()
+      }
+    })
+  }
+}
 
 function start() {
-  config.query('SELECT * FROM products', function (e, res) {
-        if (e) throw e
-        console.log('Welcome to Bamazon! Here is what we have in stock:')
+  selectInventory()
+}
 
-        for (var i = 0; i < res.length; i++) {
-          console.log('\n' + 'ID: ' + res[i].item_id + ' | ' + 'PRODUCT: ' + res[i].product_name + ' | ' + 'DEPARTMENT: ' + res[i].department_name +
-            ' | ' + 'PRICE: ' + res[i].price + ' | ' + 'QTY: ' + res[i].stock_quantity + '\n')
-        }
-
-        console.log(' ')
-        inquire.prompt([{
-            type: 'input',
-            name: 'id',
-            message: 'What is the ID of the product you would like to buy?',
-
-            validate: function (value) {
-              if (isNaN(value) == false && parseInt(value) <= res.length && parseInt(value) > 0) {
-                return true
-              } else {
-                return false
-              }
-            }
-          },
-          {
-            type: 'input',
-            name: 'qty',
-            message: 'How many would you like to purchase?',
-            validate: function (value) {
-              if (isNaN(value)) {
-                return false
-              } else {
-                return true
-              }
-            }
-          }
-        ]).then(function (ans) {
-          var itemToBuy = (ans.id) - 1
-          var howMany = parseInt(ans.qty)
-          var total = parseFloat(((res[itemToBuy].price) * howMany).toFixed(2))
-
-          if (res[itemToBuy].stock_quantity >= howMany) {
-            config.query('UPDATE products SET ? WHERE ?', [{
-                  stock_quantity: (res[itemToBuy].stock_quantity - howMany)
-                },
-                {
-                  item_id: ans.id
-                }
-              ],
-              function (e, res) {
-                if (e) throw e
-                console.log('Thank for your order. Your total is $' + total.toFixed(2))
-              })
-          }
-          // config.query('SELECT * FROM department_name', function (e, deptRes){
-          //   if(e) throw e
-          //   var index
-          //   for(var i = 0; i <deptRes.length; i++){
-          //     if(deptRes[i].department_name === res[itemToBuy].department_name){
-          //       index = i
-          //     }
-          //   } 
-          else {
-            console.log('Sorry, your item is not in stock.')
-          }
-          reprompt()
-        })
-      })
-  
-      function reprompt() {
-        inquirer.prompt([{
-          type: 'confirm',
-          name: 'reply',
-          message: 'Would you like to purchase another item?'
-        }]).then(function (ans) {
-          if (ans.reply) {
-            start()
-          } else {
-            console.log('Thank you for shopping!')
-          }
-        })
-      }
-    }
-      start()
-      // config.end(function (e) {
-      //   if (e) throw e
-      // })
+start()
--- bamazon_schema.sql
+++ bamazon_schema.sql
@@ -12,16 +12,16 @@ stock_quantity INTEGER(10) NOT NULL
 
 INSERT INTO products (item_id, product_name, department, price, stock_quantity)
 VALUES 
-('001','Gibson Les Paul Custom', 'Guitars', 4999.99, 5),
-('002','Gibson SG', 'Guitars', 1499.99, 3),
-('003','Fender Jazzmaster', 'Guitars', 1299.99, 2),
-('004','Fender Jazz Bass', 'Basses', 899.99, 4),
-('005','G&L L-2000', 'Basses', 1299.99, 1),
-('006','Ludwig Vistalite 3 Piece Drum Set', 'Drums', 1799.99, 3),
-('007','Ludwig Maple 4 Piece Drum Set', 'Drums', 3999.99, 1),
-('008','Marshall JCM 900', 'Guitar Amplifiers', 1199.99, 4),
-('009','Orange Tiny Terror', 'Guitar Amplifiers', 799.99, 2),
-('010','Ampeg V-4', 'Guitar Amplifiers', 599.99, 1);
+('001','Gibson Les Paul Custom', 'Guitars', 4999.99, 50),
+('002','Gibson SG', 'Guitars', 1499.99, 30),
+('003','Fender Jazzmaster', 'Guitars', 1299.99, 20),
+('004','Fender Jazz Bass', 'Basses', 899.99, 40),
+('005','G&L L-2000', 'Basses', 1299.99, 10),
+('006','Ludwig Vistalite 3 Piece Drum Set', 'Drums', 1799.99, 10),
+('007','Ludwig Maple 4 Piece Drum Set', 'Drums', 3999.99, 10),
+('008','Marshall JCM 900', 'Guitar Amplifiers', 1199.99, 15),
+('009','Orange Tiny Terror', 'Guitar Amplifiers', 799.99, 20),
+('010','Ampeg V-4', 'Guitar Amplifiers', 599.99, 5);
 
 
 
